8a936b86265a53abab62f09241b2fb4d7d4ca455
Jan <jan.krecke@pm.me>
2022-05-06 20:47:04 +1200

    created basic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question at {question_id}.")
+
+def result(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
